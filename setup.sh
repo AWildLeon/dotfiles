af8a2fd16ee6aba152e961101996e83b7950408e
Leon Hubrich <mail@leon-hubrich.de>
2023-05-21 00:05:54 +0200

    Add Setup Logic if dir is not home

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
+
+#check if dir is ~
+if [ "$PWD" != "$HOME" ]; then
+    mv .* ~
+    mv * ~
+    cd ~
+fi
+
 echo "Installing Oh My Zsh..."
 mv .zshrc .zshrc.bak
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 mv .zshrc.bak .zshrc
 
 echo "Done!"
+
 rm ./setup.sh
 rm README.md
+rm -rf .git
